ff5d2d8b8d73590926371f6de9a47475f272f322
vioart <vioarvenda7@gmail.com>
2025-07-10 22:32:00 +0700

    Add Main Page

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express();
-const port = 3000;
+const port = 8000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
